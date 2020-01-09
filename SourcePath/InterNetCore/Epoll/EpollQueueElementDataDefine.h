769c3f187fe59626f2e14b4789ed2d0afda2c856
ShawnLee <54459770@qq.com>
2020-01-09 23:43:44 +0800

    修改Linux编译错误

--- SourcePath/InterNetCore/Epoll/EpollQueueElementDataDefine.h
+++ SourcePath/InterNetCore/Epoll/EpollQueueElementDataDefine.h
@@ -10,8 +10,8 @@
 
 #pragma region Gloabl define
 
-#define CORE_SOCKETADDR_IN socketaddr_in
-#define CORE_SOCKETADD socketaddr
+#define CORE_SOCKETADDR_IN sockaddr_in
+#define CORE_SOCKETADD sockaddr
 #define CORE_SOCKET SI32
 
 #define LISTEN_LINK_COUNT 4096
