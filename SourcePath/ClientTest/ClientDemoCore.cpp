fa4b6f2c8fad8ca4c2b6f703b3cee7034191070b
ShawnLee <54459770@qq.com>
2020-01-14 17:00:25 +0800

    Add Windows Test Demo Project

---
+++ SourcePath/ClientTest/ClientDemoCore.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include "ClientDemoCore.h"
+
+
+ClientDemoCore::ClientDemoCore(): m_Sock(0), m_bInitial(false)
+{}
+
+ClientDemoCore::~ClientDemoCore()
+{
+	SAFE_RELEASE_SOCKET(m_Sock);
+}
+
+bool ClientDemoCore::Initialize()
+{
+	return m_bInitial;
+}
+
+bool ClientDemoCore::Start()
+{
+	return true;
+}
+
+bool ClientDemoCore::MainLoop()
+{
+	return false;
+}
+
+bool ClientDemoCore::CreateServerLink()
+{
+	return false;
+}
+
+bool ClientDemoCore::ReciveMsg()
+{
+	return false;
+}
+
+bool ClientDemoCore::SendMsg()
+{
+	return false;
+}
